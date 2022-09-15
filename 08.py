ae868303c0e8e7640ea3635e39150dfe4545d9c6
Gustavo de Oliveira Silva Hilario Amorim <105814628+Gustavo-OSHA@users.noreply.github.com>
2022-09-15 14:04:42 -0300

    Update 08.py

--- 08.py
+++ 08.py
@@ -26,11 +26,3 @@ if __name__=='__main__':
 
     if escolha == '2':
         fahr_cel()
-
-"""
-Neste código criamos duas funções distintas de conversão: cel_fahr() e fahr_cel(), que realizam as conversões de celsius para fahrenheit e de fahrenheit para celsius,
-respectivamente. Também criamos um menu de opções para que o usuário possa escolher o tipo de conversão desejada. Usamos a função input() para capturar o valor digitado
-pelo usuário, e o convertemos para o tipo float antes de armazená-lo em uma variável de cálculo.
-
-No programa principal simplesmente executamos o menu e chamamos a função escolhida pelo usuário.
-"""
